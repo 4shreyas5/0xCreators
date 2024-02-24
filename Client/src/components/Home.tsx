fbdbb6abd55fa7cd5ce9a17a2821e7e68c2a74ad
Vineet Kumar Kankerwal <codevkankerwal@gmail.com>
2024-02-25 05:03:59 +0530

    front end without routing

---
+++ Client/src/components/Home.tsx
@@ -0,0 +1,10 @@
+const home: React.FC = () => {
+   
+  return (
+    <div className="navbar navbar-dark bg-dark fixed-top">
+      hello world from home
+    </div>
+  );
+};
+
+export default home;
